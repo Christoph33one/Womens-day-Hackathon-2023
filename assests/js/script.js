df8321c9db24b152c555919b3c5a0fb791dd8d66
ARcKP98 <pkartik4386@outlook.com>
2023-03-17 16:09:58 +0000

    Add a back-to-top button.

---
+++ assests/js/script.js
@@ -0,0 +1,24 @@
+//Get the button
+let mybutton = document.getElementById("back-to-top-button");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 30 ||
+    document.documentElement.scrollTop > 30
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
